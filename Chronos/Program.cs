af89f910025a8b635a94cbf1d50aa0ab33ad0164
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-25 15:48:19 +0000

    Coments added to Chrono

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -8,25 +8,32 @@ namespace Chronos
     {
         private static void Main(string[] args)
         {
+
+            //Inicialize cronometors
             Stopwatch crono1 = new Stopwatch();
 
             Stopwatch crono2 = new Stopwatch();
 
+
+            //Start and pause cronometor1
             crono1.Start();
             Thread.Sleep(500);
             
-
+            //Start and pause cronometor2
             crono2.Start();
             Thread.Sleep(250);
+            
+            //Stop conometors
             crono1.Stop();
             crono2.Stop();
 
+            //Get the elapsed time as timespan value
             TimeSpan ts1 = crono1.Elapsed;
             TimeSpan ts2 = crono2.Elapsed;
 
 
 
-
+            //Format and display the timespan value
             string elapsedTime1 = string.Format("{0.0000}: {1.0000}: {2.0000}: {3.0000}",
             ts1.Hours, ts1.Minutes, ts1.Seconds, ts1.Milliseconds / 10 );
 
