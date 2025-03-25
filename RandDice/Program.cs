64c5872fc1dc2d1314acfef913e152847451d7c8
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-25 17:46:11 +0000

    Changes1 to program Rand

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,7 +6,23 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Random dice = new Random(6);
+
+            for (int n=0; n<=6; n++)
+                Console.WriteLine(dice.Next());
+            
+             
+            /*
+            Random r1 = new Random(22);
+            Random r2 = new Random(22);
+            
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(r1.Next());
+            Console.WriteLine();
+            for (int i = 0; i < 4; i++)
+                Console.WriteLine(r2.Next());
+            */
+
         }
     }
 }
