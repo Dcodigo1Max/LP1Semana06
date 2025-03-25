1b2b8e6a52b60fa58692432f06d294e5b90975e5
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-25 16:33:40 +0000

    Summary Added

--- RandomTest/Program.cs
+++ RandomTest/Program.cs
@@ -4,15 +4,29 @@ namespace RandomTest
 {
     public class Program
     {
+
+        /// <summary>
+        /// Este codigo faz o random das variaveis pela seed e apresenta o seu resultado
+        /// Anotações:
+        /// 1:
+        /// 2:
+        /// 3:Os numeros são sempre os mesmos, em todos os testes do codigo 
+        /// 4: Alterar o numero altera a sequencia
+        /// 5: Sim, mudar um seed não muda o resultado da outra
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
-            Random r1 = new Random(20);
+            Random r1 = new Random(22);
             Random r2 = new Random(22);
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r1.Next());
             Console.WriteLine();
             for (int i = 0; i < 4; i++)
                 Console.WriteLine(r2.Next());
+
+
+
         }
     }
 }
