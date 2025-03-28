310984a3ec1275ca9219b2912b30881cb374bd8f
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-28 16:47:08 +0000

    First Commit

---
+++ MyRougueLike/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyRougueLike
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
