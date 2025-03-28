09cb8d86732a11791781013bf7dbe8b893370c9f
Dcodigo1Max <davidrasinhas.info@gmail.com>
2025-03-28 16:54:33 +0000

    C files added

---
+++ MyRougueLike/Enemy.cs
@@ -0,0 +1,122 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+namespace MyGame
+{    
+
+    public enum PowerUp
+        {
+            Health,
+            Shield
+        }
+
+   public class Enemy
+    {
+        private string name;
+        private float health;
+        private float shield;
+
+
+        public Enemy(string name)
+        {
+        
+            SetName(name);
+            health = 100;
+            shield =0;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetHealth()
+        {
+            return Convert.ToString(health);
+        }
+
+         public string GetShield()
+        {
+            return Convert.ToString(shield);
+        }
+
+        
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield<0)
+            {
+                float damageStillToinflict =-shield;
+                shield = 0;
+                health -= damageStillToinflict;
+                if (health<0) health = 0;
+            }
+        }
+
+        public void SetName(string name)
+        {
+            this.name=name;
+            if ( name.Length >8)
+            {
+                Console.WriteLine(name.Remove(9));
+            }
+            else
+                {
+                    Console.WriteLine(name);
+                }
+        }
+
+        public static int GetPowerUps()
+        {
+            return powerUpsCollected;
+        }
+        
+        static Enemy()
+        {
+            powerUpsCollected = 0;
+        }
+
+
+
+        public void PickupPowerUp(PowerUp powerUp ,float value)
+        {
+            if (powerUp == PowerUp.Health)
+            {
+                health = health + value;
+                if (health > 100)
+                {
+                    health = 100;
+                }
+            }
+        
+
+            if (powerUp == PowerUp.Shield)
+            {
+                shield = shield + value;
+                if (shield > 100)
+                {
+                    shield = 100;
+                }
+                
+                
+            
+            }
+           
+           powerUpsCollected++; 
+            
+            
+        } 
+
+
+
+
+
+    } 
+    
+
+}
+
--- MyRougueLike/Program.cs
+++ MyRougueLike/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
-namespace MyRougueLike
+namespace MyGame
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int size = int.Parse(args[0]);
+
+            Enemy[]array = new Enemy[size];
+
+            for(int i = 0; i<size; i++)
+            {
+                Console.Write($"Nome do inimigo {i + 1}:");
+                string name = Console.ReadLine();
+                array[i] = new Enemy(name);
+            }
+
+              foreach (Enemy name in array)
+            {
+                Console.WriteLine($"{name.GetName()} {name.GetHealth()} {name.GetShield()} ");
+            }
+
+            array[0].PickupPowerUp(PowerUp.Health, 20);
+            array[0].PickupPowerUp(PowerUp.Shield, 30);
+            array[1].TakeDamage(40);
+            
+            foreach (Enemy name in array)
+            {
+
+                Console.WriteLine($"{name.GetName()} {name.GetHealth()} {name.GetShield()}");
+            }
+
+            Console.WriteLine($"PowerUps Collected : {Enemy.GetPowerUps()}");
+
+            
+            
+
         }
     }
 }
